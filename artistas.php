06783de93124e8c41e6e38d2ef443dff441caf01
vilachocolate <vilasalgada@gmail.com>
2024-09-03 11:36:29 -0300

    alteração

---
+++ artistas.php
@@ -0,0 +1,29 @@
+<?php 
+    $db = new mysqli("localhost","root","", "discoteca");
+    $query=  "select * from artista";
+    $resultado = $db->query($query);
+
+    echo "<table>";
+    echo "<tr> 
+            <td>Nome</td>
+            <td>ID</td>
+            <td>fazer</td>
+        </tr>";
+    
+    if($resultado ->num_rows==0){
+        echo"não tem artistas cadastrados";
+    }else{
+        foreach($resultado as $linha){
+            echo "<tr>";
+            echo "<td> {$linha['Nome']}</td>";
+            echo "<td> {$linha['IdArtista']}</td>";
+            //echo "<td> <a href='delArtista.php?idArtista={$linha['IdArtista']}>Eliminar</a>";
+            echo "</tr>";
+        }
+    }
+    echo "</table>";
+    echo "<br>";
+    echo "<a href='form_addArtista.php'>adicionar artista</a>";
+    echo "<br>";
+    echo "<a href='index.php'>Voltar</a>"
+?>
---
+++ form_addArtista.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>adicinar artista</title>
+</head>
+<body>
+    <form action="addArtista.php" method="post">
+        <label  for="Nome" >Nome:</label>
+        <input type="text" id="Nome" name='Nome' required>
+        <br>
+        <input type="submit" value="adicinar" name="botao">
+    </form>
+</body>
+</html>
