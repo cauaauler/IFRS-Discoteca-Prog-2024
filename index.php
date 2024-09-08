781da0e3eb86b9a2d86551cc7a83ad78e1f9fa34
vilachocolate <161588372+vilachocolate@users.noreply.github.com>
2024-09-08 16:32:58 -0300

    devolver

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
-<?php
-echo "bom dia";
-echo "</br>";
-echo "<a href='artistas.php'>ver artistas</a>";
-echo "<br>";
-echo "<a href='discos.php'>ver discoteca</a>"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BASE</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+     echo "bom dia";
+     echo "</br>";
+     echo "<a href='artistas.php'>ver artistas</a>";
+     echo "<br>";
+     echo "<a href='discos.php'>ver discoteca</a>"
+    ?>
+    
+</body>
+</html>
 
