9e0ff58a25d45a5b10e20f11c46b654a74501a9e
vilachocolate <161588372+vilachocolate@users.noreply.github.com>
2024-09-08 19:11:46 -0300

    devolver

--- devolucaoDisco.php
+++ devolucaoDisco.php
@@ -1,17 +1,29 @@
 <?php
+    if(isset($_GET['IdDisco'])){
     $db = new mysqli("localhost", "root", "", "discoteca");
     if ($db->connect_error) {
         die("Connection failed: " . $db->connect_error);
     }
 
+    //coloca em uma variavel separada o id do disco
     $idDisco = intval($_GET['IdDisco']);
 
+
+    //pega os codigos de cada um
     $querry= "UPDATE disco set Emprestado = 0 where IdDisco={$idDisco}";
     $querry2= "UPDATE emprestimo set devolvido=1 where IdDisco={$idDisco}";
+
+    //executa ambos os comandos
     $resultado=$db -> query($querry);
     $resultado=$db -> query($querry2);
 
-    -
-    header('location:discos.php')
+    
 
+    //aqui vai voltar para a lista de discos
+    header('location:discos.php');
+    $db->close();
+}
+    else(
+        header("location:erroDev")
+    )
 ?>
