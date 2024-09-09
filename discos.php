106de1a3b5d1b67936d4cfe84f9f5e281b51f9ae
Cauã Auler <cauaauler@gmail.com>
2024-09-09 16:12:37 -0300

    alteração no css

--- discos.php
+++ discos.php
@@ -53,7 +53,7 @@ $db->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="discos.css">
 </head>
 
 <body>
