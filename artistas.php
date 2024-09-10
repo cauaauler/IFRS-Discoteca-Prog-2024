452135b0c95805be611fcba5da80873e99fcacab
Cauã Auler <cauaauler@gmail.com>
2024-09-10 18:16:52 -0300

    css

--- artistas.php
+++ artistas.php
@@ -1,6 +1,6 @@
 <?php
 $db = new mysqli("localhost", "root", "", "discoteca");
-$query =  "select * from artista";
+$query =  "SELECT * FROM artista";
 $resultado = $db->query($query);
 ?>
 
@@ -12,7 +12,6 @@ $resultado = $db->query($query);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Artistas</title>
     <link rel="stylesheet" href="style.css">
-
 </head>
 
 <body>
@@ -28,33 +27,36 @@ $resultado = $db->query($query);
                 <label for="Nome">Nome:</label>
                 <input type="text" id="Nome" name='Nome' required>
                 <br>
-                <input type="submit" value="adicionar" name="botao">
+                <input type="submit" value="Adicionar" name="botao">
             </form>
         </div>
 
-        <div class="listArtista">
+        <div class="list">
             <?php
-            echo "<table border='1'>";
-            echo "<tr> 
-            <td>Nome</td>
-            <td>Ações</td>
-                </tr>";
+            echo "<table class='table'>";
+            echo "<thead><tr> 
+            <th>Nome</th>
+            <th>Ações</th>
+            </tr></thead><tbody>";
 
             if ($resultado->num_rows == 0) {
-                echo "não tem artistas cadastrados";
+                echo "<tr><td colspan='2'>Não tem artistas cadastrados</td></tr>";
             } else {
                 foreach ($resultado as $linha) {
                     echo "<tr>";
                     echo "<td> {$linha['Nome']}</td>";
-                    echo "<td> <a href='delArtista.php?IdArtista={$linha['IdArtista']}'>Eliminar</a> 
-                   <a href='form_editArtista.php?idArtista={$linha['IdArtista']}'>Editar</a></td>";
+                    echo "<td> 
+                            <a href='delArtista.php?IdArtista={$linha['IdArtista']}'>Eliminar</a> 
+                            <a href='form_editArtista.php?IdArtista={$linha['IdArtista']}'>Editar</a>
+                        </td>";
                     echo "</tr>";
                 }
             }
 
-            echo "</table>";
+            echo "</tbody></table>";
             ?>
         </div>
     </div>
 </body>
+
 </html>
