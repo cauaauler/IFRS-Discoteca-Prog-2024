02ba2aed6e3b2a2bb8e3e51c156befb5441db4e5
Diogo Vettorazzi <69004486+D1zzi@users.noreply.github.com>
2024-09-10 20:40:35 -0300

    icone 1

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Discoteca</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="http://localhost/Discoteca-Prog-2024/uploads/certificado_ouro.png?v=2" type="image/png">
 </head>
 
 <body>
